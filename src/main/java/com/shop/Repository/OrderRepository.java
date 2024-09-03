38158a618d150f7116d5cd7738dec4d787670aa2
kyerim1 <nilzero25@gmail.com>
2024-09-03 11:08:19 +0900

    7일 주문내역

--- src/main/java/com/shop/Repository/OrderRepository.java
+++ src/main/java/com/shop/Repository/OrderRepository.java
@@ -1,9 +1,23 @@
 package com.shop.Repository;
 
 import com.shop.Entity.Order;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    // 현재 로그인한 회원의 계정으로 주문내역 조회 하기
+    @Query("select od from Order od where od.member.userId = :userId " +
+            "order by od.orderDate desc")
+    List<Order> findByOrders(@Param("userId") String userId, Pageable pageable);
+
+
+    // 현재 로그인한 회원의 주문 총 갯수 가져오기 - 페이징을 위해서 전체갯수가 얼마인지
+    @Query("select COUNT(od) from Order od where od.member.userId = :userId")
+    Long countOrder(@Param("userId") String userId);
 }
--- src/main/java/com/shop/Service/OrderService.java
+++ src/main/java/com/shop/Service/OrderService.java
@@ -2,14 +2,13 @@ package com.shop.Service;
 
 import com.shop.Dto.OrderDto;
 import com.shop.Dto.OrderHistDto;
-import com.shop.Entity.Item;
-import com.shop.Entity.Member;
-import com.shop.Entity.Order;
-import com.shop.Entity.OrderItem;
+import com.shop.Dto.OrderItemDto;
+import com.shop.Entity.*;
 import com.shop.Repository.*;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -44,6 +43,31 @@ public class OrderService {
         return order.getId();
     }
 
-    public Page<OrderHistDto> getOrderList(String name, Pageable pageable) {
+    public Page<OrderHistDto> getOrderList(String userId, Pageable pageable) {
+        // 현재 로그인한 회원의 주문내역 가져오기 - 페이징 갯수만큼(5개)
+        List<Order> orders = orderRepository.findByOrders(userId, pageable);
+        Long total = orderRepository.countOrder(userId);
+
+        List<OrderHistDto> orderHistDtoList = new ArrayList<>();
+        // 현재 로그인한 회원정보
+        Member member = memberRepository.findByUserId(userId);
+
+        for(Order order : orders){
+            OrderHistDto orderHistDto = new OrderHistDto(order, member);
+            //주문 상품들 entity->DTO
+            List<OrderItem> orderItemList = order.getOrderItemList();
+            for(OrderItem orderItem : orderItemList){
+                // 주문 상품의 대표 이미지 가져오기 - 주문상품의 상품 번호와 Y
+                ItemImg itemImg = itemImgRepository
+                        .findByItemIdAndRepImgYn(orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto =
+                        new OrderItemDto(orderItem,itemImg.getImgUrl());
+                // 주문내역DTO에 주문상품DTO 저장 - ArrayList에 add해서 저장하기
+                orderHistDto.addOrderItemDto( orderItemDto);
+            }
+            orderHistDtoList.add( orderHistDto );
+        }
+        // 페이징을 위해 PageImpl 인터페이스로 반환
+        return new PageImpl<>(orderHistDtoList, pageable, total);
     }
 }
